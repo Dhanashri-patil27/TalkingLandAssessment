36b8060d53eb8fd6e76b749c1516985178af5a68
DhanashriSP <dhanashri.patil@5gencare.com>
2024-11-18 20:25:56 +0530

    Fixes #2.. Updating docker files

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,16 @@ WORKDIR /app
 # Copy package.json and install dependencies
 COPY package*.json ./
 RUN npm install \
-    && npm install -g sequelize-cli
+    && npm install -g dotenv sequelize sequelize-cli
+
+# Check that dotenv is installed
+RUN ls -l /app/node_modules | grep dotenv
 
 # Copy application code
 COPY . .
+
+# Ensure .env file is copied
+COPY .env .env
 
 # Make the entrypoint script executable
 RUN chmod +x entrypoint.sh
--- config/config.js
+++ config/config.js
@@ -1,31 +1,35 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log(process.env.POSTGRES_DB); // Debugging line
 module.exports = {
-
     development: {
         dialect: 'postgres',
-        "database": process.env.POSTGRES_DB,
-        "username": process.env.POSTGRES_USER,
-        "password": process.env.POSTGRES_PASSWORD,
-        "host": process.env.POSTGRES_HOST || 'postgres', // Change localhost to postgres,
-
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        host: process.env.POSTGRES_HOST || 'postgres', // Defaults to 'postgres' if POSTGRES_HOST is not set
+        port: process.env.DB_PORT || 5432, // Ensure the port is correctly set, default to 5432
+        logging: console.log, // Enable logging for debugging
     },
 
     test: {
-        "dialect": 'postgres',
+        dialect: 'postgres',
         database: process.env.POSTGRES_DB,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        host: process.env.POSTGRES_HOST || 'postgres', // Change localhost to postgres,
+        host: process.env.POSTGRES_HOST || 'postgres',
+        port: process.env.DB_PORT || 5432,
+        logging: false, // Disable logging for tests
     },
 
     production: {
-        "dialect": 'postgres',
+        dialect: 'postgres',
         database: process.env.POSTGRES_DB,
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        host: process.env.POSTGRES_HOST || 'postgres', // Change localhost to postgres,
+        host: process.env.POSTGRES_HOST || 'postgres',
+        port: process.env.DB_PORT || 5432,
+        logging: false, // Disable logging for production
     },
-
 };
